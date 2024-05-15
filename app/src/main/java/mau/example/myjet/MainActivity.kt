ee2d6b3926e3efb8b56455a527c943b83c9886ea
Mauricio <stb539@gmail.com>
2024-05-15 10:23:05 -0400

    iniciando

--- app/src/main/java/mau/example/myjet/MainActivity.kt
+++ app/src/main/java/mau/example/myjet/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,7 +34,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            //Text("Hola Jetpack Compose")
             //MyText("Hola Jetpack Compose")
             //MyText("Preparado")
 //            MyImage()
